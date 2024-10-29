7f67810f9cc93b25d259fbeb96e82ba236dbf82b
ATOROYO <davidatoroyosika@gmail.com>
2024-10-29 19:29:26 +0300

    Wrote code to confirm user already exist

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -18,7 +18,11 @@ exports.registerPatient = async (req, res) => {
   const { firstName, lastName, email, phone, password } = req.body;
 
   try{
-
+    // Check if patient exist
+    const [patient] = await db.execute("SELECT email FROM patients WHERE EMAIL = ?", [email]);
+    if (patient.length > 0) {
+        return res.status(400).json({message:"The user already exist! "})
+    }
   }catch(){
       
   }
