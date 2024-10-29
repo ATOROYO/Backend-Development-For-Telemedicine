46044099196f1ab00050b8a89c928081ee8e3df8
ATOROYO <davidatoroyosika@gmail.com>
2024-10-29 17:53:30 +0300

    Configured the session

--- app.js
+++ app.js
@@ -12,3 +12,12 @@ const app = express();
 // Configure middleware
 app.use(bodyParser.json()); // Use json
 app.use(bodyParser.urlencoded({ extended: true })); // Capture form data
+
+// Configure session
+const sessionStore = new MySQLStore({
+  host: process.env.DB_HOST,
+  port: process.env.PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
