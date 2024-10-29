1159b55cd91f7e8d0403ba88ecf12eb971d63602
ATOROYO <davidatoroyosika@gmail.com>
2024-10-29 17:58:51 +0300

    Configured the session

--- app.js
+++ app.js
@@ -21,3 +21,16 @@ const sessionStore = new MySQLStore({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
+
+// Configure Session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60, // 1 Hour
+    },
+  })
+);
