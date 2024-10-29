977beaa4e92a9e99ff2876b300a56a8c49690ba0
ATOROYO <davidatoroyosika@gmail.com>
2024-10-29 19:47:29 +0300

    Wrote code for the error message

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -14,25 +14,39 @@ exports.registerPatient = async (req, res) => {
       .json({ message: "Please correct input errors", errors: errors.array() });
   }
 
-// Fetching input parameters from the request body
+  // Fetching input parameters from the request body
   const { firstName, lastName, email, phone, password } = req.body;
 
-  try{
+  try {
     // Check if patient exist
-    const [patient] = await db.execute("SELECT email FROM patients WHERE EMAIL = ?", [email]);
+    const [patient] = await db.execute(
+      "SELECT email FROM patients WHERE EMAIL = ?",
+      [email]
+    );
     if (patient.length > 0) {
-        return res.status(400).json({message:"The user already exist! "})
+      return res.status(400).json({ message: "The user already exist! " });
     }
 
-// Prepare our data - hash the password
-const hashedPassword = await bcrypt.hash(password,10);
+    // Prepare our data - hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-// Insert the record
-awaitdb.execute("INSERT INTO patients (first_name,last_name, email, phone, password) VALUES (?,?,?,?,?", [firstName,lastName,email,phone,password])
+    // Insert the record
+    awaitdb.execute(
+      "INSERT INTO patients (first_name,last_name, email, phone, password) VALUES (?,?,?,?,?",
+      [firstName, lastName, email, phone, password]
+    );
 
-// Response
-return res.status(201).json({message:"New user registered successfully."})
-  }catch(){
-      
+    // Response
+    return res
+      .status(201)
+      .json({ message: "New user registered successfully." });
+  } catch (error) {
+    console.error(error);
+    res
+      .status()
+      .json({
+        message: "An error occured during registration",
+        error: error.message,
+      });
   }
 };
