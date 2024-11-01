e5901aaa42e0b7217128290a31a94e24ccca4d29
ATOROYO <davidatoroyosika@gmail.com>
2024-11-01 14:41:38 +0300

    Created registerPatientRoute

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -10,10 +10,14 @@ const { check } = require("express-validator");
 const router = express.Router();
 
 // Registration
-router.post("/register", [
-  check("first_name", "First name is require").not().isEmpty(),
-  check("last_name", "Last name is require").not().isEmpty(),
-  check("email", "Please enter a valid email").isEmail(),
-  check("phone", "Please enter a valid phone number").matches(/^\d{10}$/),
-  check("password", "Please must be 6 characters long").isLength({ min: 6 }),
-]);
+router.post(
+  "/register",
+  [
+    check("first_name", "First name is require").not().isEmpty(),
+    check("last_name", "Last name is require").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check("phone", "Please enter a valid phone number").matches(/^\d{10}$/),
+    check("password", "Please must be 6 characters long").isLength({ min: 6 }),
+  ],
+  registerPatient
+);
