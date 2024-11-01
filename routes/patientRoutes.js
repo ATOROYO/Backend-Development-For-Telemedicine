afca857f2bd0e4e8832e5cb74eb2d7567b839088
ATOROYO <davidatoroyosika@gmail.com>
2024-11-01 14:21:15 +0300

    Imported login credentials

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,2 +1,8 @@
 // Imported modules
 const express = require("express");
+const {
+  registerPatient,
+  loginPatient,
+  logoutPatient,
+  updatePateint,
+} = require("../controllers/patientController");
