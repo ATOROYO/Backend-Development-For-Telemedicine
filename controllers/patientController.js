3d78c73899968a3b0727fabf33cfa439fa5872e2
ATOROYO <davidatoroyosika@gmail.com>
2024-11-05 19:59:09 +0300

    Made a code to the session inthe controller

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -74,7 +74,7 @@ exports.loginPatient = async () => {
     }
 
     // Create a session
-    res.session.patientId = patient[0].patientId;
+    req.session.patientId = patient[0].patientId;
     res.session.firstName = patient[0].firstName;
     res.session.lastName = patient[0].lastName;
     res.session.email = patient[0].email;
