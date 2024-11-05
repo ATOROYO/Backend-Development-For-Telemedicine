14f09d9c1edf5332c8bc31582063aefb853d9f9f
ATOROYO <davidatoroyosika@gmail.com>
2024-11-05 19:55:22 +0300

    Added a code for the session

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -76,6 +76,7 @@ exports.loginPatient = async () => {
     // Create a session
     res.session.patientId = patient[0].patientId;
     res.session.firstName = patient[0].firstName;
+    res.session.lastName = patient[0].lastName;
     res.session.email = patient[0].email;
 
     return res.status.json({ message: "Successfull login" });
