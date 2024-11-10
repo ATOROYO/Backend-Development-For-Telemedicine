3f05e9e810de57a9eba3c1a933dfa930e7f5ada0
ATOROYO <davidatoroyosika@gmail.com>
2024-11-10 15:40:22 +0300

    Added variables to script.js

---
+++ frontend/js/script.js
@@ -0,0 +1,7 @@
+// Declaring variable
+const divMessage = document.getElementById('message');
+const patientSection = document.getElementById('patientSection');
+const pFirstNameSpan = document.getElementById('pFirstName');
+const pLastNameSpan = document.getElementById('pLastName');
+const patientEmail = document.getElementById('pEmail');
+const logoutButton = document.getElementById('logoutbutton');
