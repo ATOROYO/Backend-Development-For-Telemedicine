19c111d2b021633e6b757389d6d546072fbae2bc
ATOROYO <davidatoroyosika@gmail.com>
2024-11-15 17:37:10 +0300

    installed some packages

--- app.js
+++ app.js
@@ -53,3 +53,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running at: http://localhost:${PORT}`);
 });
+
+console.log(dbConfig);
+console.log(sessionConfig);
