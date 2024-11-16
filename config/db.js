24667129b83bf05a688d70ad12317456aa975bb4
ATOROYO <davidatoroyosika@gmail.com>
2024-11-16 23:17:49 +0300

    Deleted a code

--- config/db.js
+++ config/db.js
@@ -7,7 +7,6 @@ dotenv.config();
 // Connection to the database
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
