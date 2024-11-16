ac51664480f89b7cccaa1034ed391a9d6160cb02
ATOROYO <davidatoroyosika@gmail.com>
2024-11-16 23:36:49 +0300

    Updated a code

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,48 +1,48 @@
 // Imported modules
-const express = require("express");
+const express = require('express');
 const {
   registerPatient,
   loginPatient,
   logoutPatient,
   updatePateint,
   getPatient,
-} = require("../controllers/patientController"); // Internal modules
-const { check } = require("express-validator"); // Validator
+} = require('../controllers/patientController'); // Internal modules
+const { check } = require('express-validator'); // Validator
 const router = express.Router(); // Help in directing requests
 
 // Registration
 router.post(
-  "/register",
+  '/register',
   [
-    check("first_name", "First name is require").not().isEmpty(),
-    check("last_name", "Last name is require").not().isEmpty(),
-    check("email", "Please enter a valid email").isEmail(),
-    check("phone", "Please enter a valid phone number").matches(/^\d{10}$/),
-    check("password", "Please must be 6 characters long").isLength({ min: 6 }),
+    check('first_name', 'First name is require').not().isEmpty(),
+    check('last_name', 'Last name is require').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('phone', 'Please enter a valid phone number').matches(/^\d{10}$/),
+    check('password', 'Please must be 6 characters long').isLength({ min: 6 }),
   ],
   registerPatient
 );
 
 // Login route
-router.post("/login", loginPatient);
+router.post('/login', loginPatient);
 
 // Get  patient route
-router.get("/patient", getPatient);
+router.get('/patient', getPatient);
 
 // Update the patient route
 router.put(
-  "/patient/update",
+  '/patient/update',
   [
-    check("first_name", "First name is require").not().isEmpty(),
-    check("last_name", "Last name is require").not().isEmpty(),
-    check("email", "Please enter a valid email").isEmail(),
-    check("phone", "Please enter a valid phone number").matches(/^\d{10}$/),
-    check("password", "Please must be 6 characters long").isLength({ min: 6 }),
+    check('first_name', 'First name is require').not().isEmpty(),
+    check('last_name', 'Last name is require').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('phone', 'Please enter a valid phone number').matches(/^\d{10}$/),
+    check('password', 'Please must be 6 characters long').isLength({ min: 6 }),
   ],
   updatePateint
 );
 
 // Logout patient route
-router.get("/logout", logoutPatient);
+router.get('/logout', logoutPatient);
 
 module.exports = router;
