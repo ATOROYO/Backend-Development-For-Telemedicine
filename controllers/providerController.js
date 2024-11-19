d008a0fbc6dec22879a5b8588c2c445275f645aa
ATOROYO <davidatoroyosika@gmail.com>
2024-11-19 23:15:40 +0300

    Imported modules in the providerController

---
+++ controllers/providerController.js
@@ -0,0 +1,4 @@
+// Imported moules
+const db = require('../config/db'); // Database connection
+const bcrypt = require('bcryptjs'); // Hashing passwords
+const { validationResult } = require('express-validator'); // Validator
