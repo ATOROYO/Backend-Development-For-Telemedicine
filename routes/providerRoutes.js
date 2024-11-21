26ff33cbec4056bfaf433179e0d57cba06c9eb57
ATOROYO <davidatoroyosika@gmail.com>
2024-11-21 18:30:35 +0300

    Imported internal modules

--- routes/providerRoutes.js
+++ routes/providerRoutes.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const { check } = require('express-validator'); // Validator
 const router = express.Router(); // Help in directing requests
+const {
+  registerPatient,
+  loginPatient,
+  logoutPatient,
+  updatePatient,
+  getPatient,
+} = require('../controllers/patientController'); // Internal modules
