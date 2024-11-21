c8848fdbfc5c03d33a99a52f30576f33c5038723
ATOROYO <davidatoroyosika@gmail.com>
2024-11-21 18:33:55 +0300

    Added a line for the specialty

--- routes/providerRoutes.js
+++ routes/providerRoutes.js
@@ -15,6 +15,7 @@ router.post(
   [
     check('first_name', 'First name is require').not().isEmpty(),
     check('last_name', 'Last name is require').not().isEmpty(),
+    check('specialty', 'Last name is require').not().isEmpty(),
     check('email', 'Please enter a valid email').isEmail(),
     check('phone', 'Please enter a valid phone number').matches(/^\d{10}$/),
     check('password', 'Please must be 6 characters long').isLength({ min: 6 }),
