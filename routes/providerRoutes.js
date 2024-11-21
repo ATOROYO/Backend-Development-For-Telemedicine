fb2eaf0f3c24c809e5f1dd25ebc1780f62304ab2
ATOROYO <davidatoroyosika@gmail.com>
2024-11-21 18:38:39 +0300

    Added route to update provider

--- routes/providerRoutes.js
+++ routes/providerRoutes.js
@@ -25,3 +25,16 @@ router.post(
 
 // Login route
 router.post('/login', loginProvider);
+
+router.put(
+  '/provider/update',
+  [
+    check('first_name', 'First name is require').not().isEmpty(),
+    check('last_name', 'Last name is require').not().isEmpty(),
+    check('specialty', 'Last name is require').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('phone', 'Please enter a valid phone number').matches(/^\d{10}$/),
+    check('password', 'Please must be 6 characters long').isLength({ min: 6 }),
+  ],
+  updateProvider
+);
