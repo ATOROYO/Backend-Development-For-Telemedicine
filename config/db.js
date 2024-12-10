e5bc3c83a465eb9f44ff78f2d76789e876d7c4b7
ATOROYO <davidatoroyosika@gmail.com>
2024-12-10 14:09:24 +0300

    Updated the database imformation

--- config/db.js
+++ config/db.js
@@ -1,15 +1,32 @@
 // Imported modules
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config();
+// Get the directory of the current file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load the .env file
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 // Connection to the database
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
 
-module.exports = pool.promise();
+async function getUser() {
+  const [rows] = await pool.query('DESC patients');
+  return rows;
+}
+
+const result = await getUser();
+console.log(result);
+
+// module.exports = pool.promise();
